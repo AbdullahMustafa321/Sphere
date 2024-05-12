f1ddbfb2c88f3a71168f1f5822a7c00e9604df01
AbdullahMustafa321 <mostafaabdala505@gmail.com>
2024-05-12 19:49:25 +0300

    create home repo method & failure class

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:sphere_book/core/errors/failure.dart';
+import '../models/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+}
