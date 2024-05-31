8a199d9ee9aaa8c95b1ddcd099404ee7d37e7ede
AbdullahMustafa321 <mostafaabdala505@gmail.com>
2024-05-31 23:38:22 +0300

    integration the stripe to my app part two

--- android/app/src/main/kotlin/com/example/sphere_book/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sphere_book/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.sphere_book
 
-import io.flutter.embedding.android.FlutterActivity
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity: FlutterFragmentActivity()
